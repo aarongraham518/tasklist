4748f9fc4938280ec6c4c773e48666723b469fa2
Aaron Graham <aarongraham518@gmail.com>
2020-01-18 12:38:42 -0500

    Added remove, clear and filter task events

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ loadEventlisteners();
 function loadEventlisteners(){
     //Add task event
     form.addEventListener('submit', addTask);
+
+    //Remove task event
+    taskList.addEventListener('click', removeTask);
+
+    //Clear Task Event
+    clearBtn.addEventListener('click', clearTasks);
+
+    //Filter tasks event
+    filter.addEventListener('keyup', filterTasks);
 }
 
 //Add Task
@@ -49,6 +58,54 @@ function addTask(e){
 
     //clear input
     taskInput.value = '';
-    
+
     e.preventDefault();
+}
+
+//Remove Task
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+        if(confirm('Are you Sure?')){
+            e.target.parentElement.parentElement.remove();
+            // can we output the text of the removed task?
+        }       
+    }    
+}
+
+//Clear Tasks
+function clearTasks(){
+    // taskList.innerHTML = '';
+
+    //Faster alternative
+    //While there is a first of .collection
+    while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+    }
+
+    //https://jsperf.com/innerhtml-vs-removechild
+}
+
+//Filter Tasks
+function filterTasks(e){
+
+    const text = e.target.value.toLowerCase();
+
+    /*
+        Returns a node list, if we use getElementByClass that
+        would return an html collection which we would have to convert
+        to an array in order to use forEach
+    */
+    
+    document.querySelectorAll('.collection-item').forEach
+    (function(task){
+
+        //task is each iteration of the .collection-item
+        //firstChil
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1){     //no match on indexOf = -1
+            task.style.display = 'block';
+        }else{
+            task.style.display = 'none';
+        }
+    });
 }
